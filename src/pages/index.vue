b995c4e60f8f1121945693c59b61a421e6299221
DK <liuxu@souche.com>
2018-06-06 18:04:48 +0800

    static路径

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <div>上传图片到腾讯云</div>
+        <div>上传图片到腾讯云（Beta）</div>
         <div>
             <el-upload ref="upload"
                 action="/test/upload"
@@ -13,8 +13,10 @@
                 <el-button type="primary" slot="trigger">选择文件（可多选）</el-button>
                 <el-button type="success" @click="submitUpload">上传全部</el-button>
                 <el-button type="danger" @click="clearAll">清空上传</el-button>
+                <el-button type="info" @click="viewAll">所有上传</el-button>
             </el-upload>
         </div>
+        <!-- <div class="">项目测试阶段，如遇上传未成功，请刷新重试。如多次未成功请联系网站管理员</div> -->
     </div>
 </template>
 
@@ -37,7 +39,20 @@ export default {
         },
         clearAll() {
             this.fileList = [];
+        },
+        viewAll() {
+            this.$message({
+                message: '功能开发中，敬请期待'
+            });
         }
+    },
+    mounted() {
+        this.$notify({
+            title: '提示',
+            message: '项目测试阶段，如遇上传未成功，请刷新重试。如多次未成功请联系网站管理员',
+            duration: 0,
+            position: 'bottom-right'
+        });
     }
 };
 </script>
